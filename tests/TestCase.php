bc65c3d30de0c862905074863d1b73965d5d96f4
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2016-06-27 22:50:36 -0300

    FIX: factory test setting.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,33 @@
 <?php
 
+use Masknizzer\EnumMasks;
+use Masknizzer\MaskFactory;
+
 class TestCase extends PHPUnit_Framework_TestCase
 {
-    public function testTesting()
+    public function genericDataProvider()
     {
-        $this->assertTrue(true);
+        return [
+            [
+                'maskFactory' => new MaskFactory(),
+                'maskEnum'    => EnumMasks::PHONE_NUMBER_8(),
+                'wordToMask'  => '53142325'
+
+            ],
+            [
+                'maskFactory' => new MaskFactory(),
+                'maskEnum'    => EnumMasks::POSTAL_CODE(),
+                'wordToMask'  => '25254121'
+
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider genericDataProvider
+     */
+    public function testFactory(MaskFactory $maskFactory, EnumMasks $maskEnum, $wordToMask){
+        $mask = $maskFactory->factory($maskEnum, $wordToMask);
+        $this->assertInstanceOf('Masknizzer\MaskField', $mask);
     }
 }
